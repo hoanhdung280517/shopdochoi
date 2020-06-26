c52dcbde4650e82f972a87847d457a8251228da0
thanhxuancoffeeandmore <thanhxuancoffeeandmore>
2020-06-26 18:31:37 +0700

    Update Config.php

--- system/core/Config.php
+++ system/core/Config.php
@@ -104,7 +104,7 @@ class CI_Config {
 			}
 			else
 			{
-				$base_url = 'http://localhost/';
+				$base_url = 'https://dungtoydochoitrecon.herokuapp.com/';
 			}
 
 			$this->set_item('base_url', $base_url);
