e3abd547661309fdf554719b06d475ef5aefa28f
thanhxuancoffeeandmore <thanhxuancoffeeandmore>
2020-06-28 08:37:41 +0700

    Update detail.php

--- application/views/frontend/components/tintuc/detail.php
+++ application/views/frontend/components/tintuc/detail.php
@@ -2,9 +2,7 @@
 	<div class="row wraper">
 		<div class="banner-product">
 		<div class="container">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<img src="public/images/sp.png">
-			</div>
+		
 		</div>
 	</div>
 		<div class="container">
